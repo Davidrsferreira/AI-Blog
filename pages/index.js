25cbf358a0240aead3dd0876009fe93d0308d7f8
David F <davidrsferreira@gmail.com>
2025-03-11 14:32:37 -0300

    added Oauth0

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+
+  console.log(user)
+  return (
+    <div>
+      <h1>Homepage</h1>
+      {!!user ? (
+        <div>
+          <div>
+            <Image
+              src={user.picture}
+              alt={user.name}
+              height={50}
+              width={50}
+            ></Image>
+          </div>
+          <div>
+            {user.name}
+          </div>
+          <Link href="/api/auth/logout">Logout</Link>
+        </div>
+      ) : (
+        <Link href="/api/auth/login">Login</Link>
+      )}
+    </div>
+  );
 }
