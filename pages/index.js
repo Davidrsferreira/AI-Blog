a0c59d610ea0f4f6a055f0a782e3663b0ab3ea0e
David F <davidrsferreira@gmail.com>
2025-03-12 13:19:05 -0300

    side bar implementation

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,7 @@
-import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import Image from "next/image";
-
 export default function Home() {
-  const { user } = useUser();
-
-  console.log(user)
   return (
     <div>
       <h1>Homepage</h1>
-      {!!user ? (
-        <div>
-          <div>
-            <Image
-              src={user.picture}
-              alt={user.name}
-              height={50}
-              width={50}
-            ></Image>
-          </div>
-          <div>
-            {user.name}
-          </div>
-          <Link href="/api/auth/logout">Logout</Link>
-        </div>
-      ) : (
-        <Link href="/api/auth/login">Login</Link>
-      )}
     </div>
   );
 }
